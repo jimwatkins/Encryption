104b7ad9bfb0fe492deac26104e263dc1d514da0
skyler <skyler@bigskyway.com>
2015-05-17 20:55:01 -0500

    Third Option Launch Screen Completed

--- app/src/main/java/net/pro/encryptor/encryption/ThirdOption.java
+++ app/src/main/java/net/pro/encryptor/encryption/ThirdOption.java
@@ -1,47 +1,27 @@
 package net.pro.encryptor.encryption;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class EncryptionActivity extends ActionBarActivity {
+public class ThirdOption extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_encryption);
+        setContentView(R.layout.activity_third_option);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_encryption, menu);
+        getMenuInflater().inflate(R.menu.menu_third_option, menu);
         return true;
     }
 
-    public void launchGame(View view) {
-        Log.i("Launching Screen", "Main Game Screen");
-        Intent intent = new Intent(this, EncryptionMethod1.class);
-        startActivity(intent);
-    }
-
-    public void launchMethod2(View view) {
-        Log.i("Launching Screen", "Main Game Screen");
-        Intent intent = new Intent(this, AESEncryption.class);
-        startActivity(intent);
-    }
-
-    public void launchThirdOption(View view) {
-        Log.i("Launching Screen", "Third Option Screen");
-        Intent intent = new Intent(this, ThirdOption.class);
-        startActivity(intent);
-    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
